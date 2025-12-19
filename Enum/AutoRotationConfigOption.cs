3695319e253f1b9acee5fb3db8d9375172741c56
Ethan Henderson <ethan@zbee.codes>
2025-12-19 15:05:26 -0700

    style(Enums): Follow new style guide

--- Enum/AutoRotationConfigOption.cs
+++ Enum/AutoRotationConfigOption.cs
@@ -11,7 +11,8 @@ namespace WrathCombo.API.Enum;
 /// </summary>
 public enum AutoRotationConfigOption
 {
-    [ConfigValueType(typeof(bool))] InCombatOnly = 0,
+    [ConfigValueType(typeof(bool))]
+    InCombatOnly = 0,
 
     [ConfigValueType(typeof(DPSRotationMode))]
     DPSRotationMode = 1,
@@ -19,35 +20,51 @@ public enum AutoRotationConfigOption
     [ConfigValueType(typeof(HealerRotationMode))]
     HealerRotationMode = 2,
 
-    [ConfigValueType(typeof(bool))] FATEPriority = 3,
+    [ConfigValueType(typeof(bool))]
+    FATEPriority = 3,
 
-    [ConfigValueType(typeof(bool))] QuestPriority = 4,
+    [ConfigValueType(typeof(bool))]
+    QuestPriority = 4,
 
-    [ConfigValueType(typeof(int))] SingleTargetHPP = 5,
+    [ConfigValueType(typeof(int))]
+    SingleTargetHPP = 5,
 
-    [ConfigValueType(typeof(int))] AoETargetHPP = 6,
+    [ConfigValueType(typeof(int))]
+    AoETargetHPP = 6,
 
-    [ConfigValueType(typeof(int))] SingleTargetRegenHPP = 7,
+    [ConfigValueType(typeof(int))]
+    SingleTargetRegenHPP = 7,
 
-    [ConfigValueType(typeof(bool))] ManageKardia = 8,
+    [ConfigValueType(typeof(bool))]
+    ManageKardia = 8,
 
-    [ConfigValueType(typeof(bool))] AutoRez = 9,
+    [ConfigValueType(typeof(bool))]
+    AutoRez = 9,
 
-    [ConfigValueType(typeof(bool))] AutoRezDPSJobs = 10,
+    [ConfigValueType(typeof(bool))]
+    AutoRezDPSJobs = 10,
 
-    [ConfigValueType(typeof(bool))] AutoCleanse = 11,
+    [ConfigValueType(typeof(bool))]
+    AutoCleanse = 11,
 
-    [ConfigValueType(typeof(bool))] IncludeNPCs = 12,
+    [ConfigValueType(typeof(bool))]
+    IncludeNPCs = 12,
 
-    [ConfigValueType(typeof(bool))] OnlyAttackInCombat = 13,
+    [ConfigValueType(typeof(bool))]
+    OnlyAttackInCombat = 13,
 
-    [ConfigValueType(typeof(bool))] OrbwalkerIntegration = 14,
+    [ConfigValueType(typeof(bool))]
+    OrbwalkerIntegration = 14,
 
-    [ConfigValueType(typeof(bool))] AutoRezOutOfParty = 15,
+    [ConfigValueType(typeof(bool))]
+    AutoRezOutOfParty = 15,
 
-    [ConfigValueType(typeof(int))] DPSAoETargets = 16,
+    [ConfigValueType(typeof(int))]
+    DPSAoETargets = 16,
 
-    [ConfigValueType(typeof(int))] SingleTargetExcogHPP = 17,
+    [ConfigValueType(typeof(int))]
+    SingleTargetExcogHPP = 17,
 
-    [ConfigValueType(typeof(bool))] AutoRezDPSJobsHealersOnly = 18,
+    [ConfigValueType(typeof(bool))]
+    AutoRezDPSJobsHealersOnly = 18,
 }
