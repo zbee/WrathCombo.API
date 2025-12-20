df92526647d5e10fa3d300310fbb7a49b2de9f18
Ethan Henderson <ethan@zbee.codes>
2025-12-20 13:47:37 -0700

    style(RawIPC): Matched line breaks to other `RawMethod` files
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- IPC/RawMethods/Utility.cs
+++ IPC/RawMethods/Utility.cs
@@ -11,17 +11,20 @@ namespace WrathCombo.API.IPC.RawMethods;
 
 public partial class RawMethod
 {
-    internal static readonly ICallGateSubscriber<float?, bool>
+    internal static readonly
+        ICallGateSubscriber<float?, bool>
         CanWeave =
             Interface!.GetIpcSubscriber<float?, bool>
                 ($"{WC}CanWeave");
 
-    internal static readonly ICallGateSubscriber<float?, float?, bool>
+    internal static readonly
+        ICallGateSubscriber<float?, float?, bool>
         CanDelayedWeave =
             Interface!.GetIpcSubscriber<float?, float?, bool>
                 ($"{WC}CanDelayedWeave");
 
-    internal static readonly ICallGateSubscriber<uint, bool?, bool?, bool>
+    internal static readonly
+        ICallGateSubscriber<uint, bool?, bool?, bool>
         ActionReady =
             Interface!.GetIpcSubscriber<uint, bool?, bool?, bool>
                 ($"{WC}ActionReady");
