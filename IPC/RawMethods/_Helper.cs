eaa9727b1cb7114d12c294fb10974ac0146a9359
Ethan Henderson <ethan@zbee.codes>
2025-12-20 12:47:18 -0700

    feat(Wrapper): Added initialization
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/RawMethods/_Helper.cs
@@ -0,0 +1,18 @@
+﻿#region
+
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace WrathCombo.API.IPC.RawMethods;
+
+public partial class RawMethod
+{
+    private const string WC = "WrathCombo.";
+
+    [PluginService]
+    private static IDalamudPluginInterface Interface { get; [UsedImplicitly] set; }
+        = null!;
+}
---
+++ Wrapper.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using Dalamud.Plugin;
+using WrathCombo.API.IPC.RawMethods;
+
+#endregion
+
+namespace WrathCombo.API;
+
+public static partial class WrathIPCWrapper
+{
+    private static bool _isInitialized;
+
+    public static void InitWrapper(IDalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<RawMethod>();
+        _isInitialized = true;
+    }
+}
