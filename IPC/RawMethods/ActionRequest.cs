87a4c5bd2b197978c91389f63b62d6efbfc532ff
Ethan Henderson <ethan@zbee.codes>
2025-12-20 13:24:55 -0700

    feat(RawIPC): Added `Wrath.IPC.ActionRequest` methods
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/RawMethods/ActionRequest.cs
@@ -0,0 +1,98 @@
+﻿#region
+
+using Dalamud.Plugin.Ipc;
+using FFXIVClientStructs.FFXIV.Client.Game;
+
+// ReSharper disable RedundantSuppressNullableWarningExpression
+// ReSharper disable StaticMemberInitializerReferesToMemberBelow
+
+#endregion
+
+namespace WrathCombo.API.IPC.RawMethods;
+
+public partial class RawMethod
+{
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<ActionType, uint, int, object>
+        RequestBlacklist =
+            Interface!.GetIpcSubscriber<ActionType, uint, int, object>
+                ($"{WC}RequestBlacklist");
+
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<ActionType, uint, object>
+        ResetBlacklist =
+            Interface!.GetIpcSubscriber<ActionType, uint, object>
+                ($"{WC}ResetBlacklist");
+
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<object>
+        ResetAllBlacklists =
+            Interface!.GetIpcSubscriber<object>
+                ($"{WC}ResetAllBlacklist");
+
+    internal static readonly
+        ICallGateSubscriber<ActionType, uint, float>
+        GetArtificialCooldown =
+            Interface!.GetIpcSubscriber<ActionType, uint, float>
+                ($"{WC}GetArtificialCooldown");
+
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<ActionType, uint, int, bool?, object>
+        RequestActionUse =
+            Interface!.GetIpcSubscriber<ActionType, uint, int, bool?, object>
+                ($"{WC}RequestActionUse");
+
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<ActionType, uint, object>
+        ResetRequest =
+            Interface!.GetIpcSubscriber<ActionType, uint, object>
+                ($"{WC}ResetRequest");
+
+    /// <remarks>
+    ///     Use
+    ///     <see cref="Dalamud.Plugin.Ipc.ICallGateSubscriber{T1,TRet}.InvokeAction(T1)">
+    ///         .InvokeAction()
+    ///     </see>
+    ///     , <see langword="void" /> return
+    /// </remarks>
+    internal static readonly
+        ICallGateSubscriber<object>
+        ResetAllRequests =
+            Interface!.GetIpcSubscriber<object>
+                ($"{WC}ResetAllRequests");
+}
