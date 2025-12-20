3be8c5ba734763bfcea3ab4bd2d8d5dd393675da
Ethan Henderson <ethan@zbee.codes>
2025-12-20 12:55:25 -0700

    docs(Wrapper): Implemented `Utility.CanWeave` doc comment
    To the degree that I would like all other wrapper methods documented.
    
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- IPC/Utility.cs
+++ IPC/Utility.cs
@@ -8,6 +8,36 @@ namespace WrathCombo.API;
 
 public static partial class WrathIPCWrapper
 {
+    /// <summary>
+    ///     Checks if an action can be used currently without clipping a GCD.
+    /// </summary>
+    /// <param name="estimatedWeaveTime">
+    ///     The animation-lock time associated with the action to be used.<br />
+    ///     If <see langword="null" />, then the default animation-lock time is used.
+    /// </param>
+    /// <returns>
+    ///     Whether an action with the weave time specified can be used currently.
+    /// </returns>
+    /// <exception cref="Error.UninitializedException">
+    ///     When <see cref="InitWrapper">InitWrapper()</see> has not been called.
+    /// </exception>
+    /// <exception cref="Error.APIBehindException">
+    ///     When this package needs updating.
+    /// </exception>
+    /// <exception cref="Error.IPCException">
+    ///     When there was a
+    ///     <see cref="Dalamud.Plugin.Ipc.Exceptions.IpcError">Dalamud IPC Error</see>
+    ///     thrown.
+    /// </exception>
+    /// <exception cref="Error.UnexpectedException">
+    ///     When WrathCombo.IPC needs updating.
+    /// </exception>
+    /// <remarks>
+    ///     Respects the user's Setting in Wrath Combo for the maximum number of
+    ///     weaves allowed per window. Can return <see langword="false" /> even if
+    ///     there is plenty of time to weave, if the user has set a lower number of
+    ///     allowed weaves.
+    /// </remarks>
     public static bool CanWeave(float? estimatedWeaveTime = null) =>
         SafeInvokeRawMethod(() => RawMethod.CanWeave.InvokeFunc(estimatedWeaveTime));
 
