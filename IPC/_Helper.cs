8cd0dada5591a2c67b0f5c55ec7ee03f575b1731
Ethan Henderson <ethan@zbee.codes>
2025-12-20 12:50:48 -0700

    feat(Wrapper): Added wrapper method for raw IPC method calls
    Handles non-initialization and categorizes all possibly encountered exceptions intour our exceptions
    
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/_Helper.cs
@@ -0,0 +1,60 @@
+﻿#region
+
+using Dalamud.Plugin.Ipc.Exceptions;
+using WrathCombo.API.Error;
+
+#endregion
+
+namespace WrathCombo.API;
+
+public static partial class WrathIPCWrapper
+{
+    private static T SafeInvokeRawMethod<T>(Func<T> ipcCall)
+    {
+        if (!_isInitialized)
+            throw new UninitializedException(
+                "WrathCombo IPC Wrapper not initialized.");
+
+        try
+        {
+            return ipcCall();
+        }
+        catch (IpcError ex) when (ex is IpcTypeMismatchError or
+                                      IpcValueNullError or
+                                      IpcLengthMismatchError)
+        {
+            throw new APIBehindException(
+                "WrathCombo.API needs updated; " +
+                "it is behind WrathCombo.IPC: invalid signatures.",
+                ex);
+        }
+        catch (IpcError ex) when (ex is IpcNotReadyError)
+        {
+            throw new UninitializedException(
+                "WrathCombo.IPC isn't ready.", ex);
+        }
+        catch (IpcError ex)
+        {
+            throw new IPCException(
+                "WrathCombo.API encountered an IPC error " +
+                "in using WrathCombo.IPC.",
+                ex);
+        }
+        catch (Exception ex)
+        {
+            throw new UnexpectedException(
+                "WrathCombo.API encountered an unexpected " +
+                "error in using WrathCombo.IPC.",
+                ex);
+        }
+    }
+
+    private static void SafeInvokeRawMethod(Action ipcCall)
+    {
+        SafeInvokeRawMethod(() =>
+        {
+            ipcCall();
+            return 0L;
+        });
+    }
+}
