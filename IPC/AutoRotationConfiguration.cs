01301dfd9226515bf3ed2de7cfa8ffca2f81b584
Ethan Henderson <ethan@zbee.codes>
2025-12-22 08:08:51 -0700

    feat(Wrapper): Added Typed Wrappers for `Wrath.IPC.AutoRotationConfiguration` methods
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ IPC/AutoRotationConfiguration.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using WrathCombo.API.Enum;
+using DPSMode = WrathCombo.API.Enum.DPSRotationMode;
+using HealerMode = WrathCombo.API.Enum.HealerRotationMode;
+
+#pragma warning disable CS0618 // Obsoletes here are likely ours, for consumers
+
+#endregion
+
+namespace WrathCombo.API;
+
+public static partial class WrathIPCWrapper
+{
+    public static object? GetAutoRotationConfigState
+        (AutoRotationConfigOption passedOption) =>
+        SafeInvokeRawMethod(() =>
+            RawMethod.GetAutoRotationConfigState.InvokeFunc(passedOption));
+
+    private static SetResult SetAutoRotationConfigState
+        (Guid lease, AutoRotationConfigOption passedOption, object value) =>
+        SafeInvokeRawMethod(() =>
+            RawMethod.SetAutoRotationConfigState
+                .InvokeFunc(lease, passedOption, value));
+
+    public static SetResult SetAutoRotationConfigState
+        (Guid lease, AutoRotationConfigOption passedOption, bool value) =>
+        SetAutoRotationConfigState(lease, passedOption, (object)value);
+
+    public static SetResult SetAutoRotationConfigState
+        (Guid lease, AutoRotationConfigOption passedOption, int value) =>
+        SetAutoRotationConfigState(lease, passedOption, (object)value);
+
+    public static SetResult SetAutoRotationConfigState
+        (Guid lease, AutoRotationConfigOption passedOption, DPSMode value) =>
+        SetAutoRotationConfigState(lease, passedOption, (object)value);
+
+    public static SetResult SetAutoRotationConfigState
+        (Guid lease, AutoRotationConfigOption passedOption, HealerMode value) =>
+        SafeInvokeRawMethod(() =>
+            RawMethod.SetAutoRotationConfigState
+                .InvokeFunc(lease, passedOption, value));
+}
