e842fa190e6bc946205d58d5f756a01db4273608
Ethan Henderson <ethan@zbee.codes>
2025-12-23 10:53:42 -0700

    feat(Wrapper): Get `PluginInterface` from EC, no longer requiring initialization
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- Wrapper.cs
+++ Wrapper.cs
@@ -1,5 +1,6 @@
 ﻿#region
 
+using System.Reflection;
 using Dalamud.Plugin;
 
 #endregion
@@ -8,7 +9,48 @@ namespace WrathCombo.API;
 
 public static partial class WrathIPCWrapper
 {
-    private static IDalamudPluginInterface? Interface;
+    private static IDalamudPluginInterface? Interface
+    {
+        get
+        {
+            if (field != null)
+                return field;
+
+            // Try to get the interface from ECommons if possible
+            try
+            {
+                const string           assemblyName = "ECommons";
+                const StringComparison lower = StringComparison.OrdinalIgnoreCase;
+                var eCommonsAsm = AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .FirstOrDefault(a =>
+                        a.GetName().Name?.Equals(assemblyName, lower) == true);
+                if (eCommonsAsm is null) return null;
+
+                const string svcFullName = "ECommons.DalamudServices.Svc";
+                var svcType = eCommonsAsm
+                    .GetType(svcFullName, throwOnError: false, ignoreCase: true);
+                if (svcType is null) return null;
+
+                var isInit = svcType.GetField("IsInitialized",
+                        BindingFlags.NonPublic | BindingFlags.Static)
+                    ?.GetValue(null) as bool?;
+                if (isInit != true) return null;
+
+                var raw = svcType.GetField("PluginInterface",
+                        BindingFlags.Public | BindingFlags.Static)
+                    ?.GetValue(null);
+                if (raw is null) return null;
+
+                return field = raw as IDalamudPluginInterface;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        set;
+    }
 
     public static void InitWrapper(IDalamudPluginInterface pluginInterface)
     {
