e0e328954da36b37414213a8c281b78693f93e78
Ethan Henderson <ethan@zbee.codes>
2026-01-19 20:18:20 -0700

    feat(Enums): Added Description Extension for `CancellationReason`
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

---
+++ Extension/CancellationReasonExtensions.cs
@@ -0,0 +1,41 @@
+﻿#region
+
+using System.ComponentModel;
+using System.Reflection;
+using WrathCombo.API.Enum;
+
+#endregion
+
+namespace WrathCombo.API.Extension;
+
+/// <summary>
+///     Extension methods for <see cref="CancellationReason" />.
+/// </summary>
+public static class CancellationReasonExtensions
+{
+    extension(CancellationReason value)
+    {
+        /// <summary>
+        ///     Gets the description attribute for the given
+        ///     <see cref="CancellationReason" />.
+        /// </summary>
+        /// <value>
+        ///     The option to get the description for.
+        /// </value>
+        /// <return>
+        ///     The description attribute contents as a string, or an empty
+        ///     string.
+        /// </return>
+        public string Description
+        {
+            get
+            {
+                var enumValue = typeof(CancellationReason)
+                    .GetField(value.ToString());
+                var attr = enumValue?
+                    .GetCustomAttribute<DescriptionAttribute>();
+                return attr?.Description ?? string.Empty;
+            }
+        }
+    }
+}
